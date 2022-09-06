634fc9e4c6a38791c052a77f8e335e445396cd1b
liuk <liukun@lianzhenglink.com>
2022-09-06 15:26:43 +0800

    引入scss

--- src/view/Welcome.tsx
+++ src/view/Welcome.tsx
@@ -1,8 +1,9 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from './Welcome.module.scss'
 export const Welcome = defineComponent({
     setup:()=> {
-        return () => <div>
+        return () => <div class={s.wrapper}>
             <header>1</header>
             <main><RouterView/></main>
             <footer>22</footer>
